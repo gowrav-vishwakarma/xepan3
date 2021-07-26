6b1dc43a6a496541810e991eb04daa5700139ffe
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-07-26 19:11:48 +0530

    Initial setup

---
+++ layouts/center.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app light>
+    <v-main>
+      <v-container bg grid-list-md text-xs-center fill-height fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="4">
+            <Nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      title: 'Center Layout',
+    }
+  },
+}
+</script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <NuxtLogo />
+    <div>{{ `${randNumber} Hi` }}</div>
+    <button @click="showMessageFromBackend">Show message from backend</button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'center',
+  data() {
+    return {
+      randNumber: 0,
+    }
+  },
+  mounted() {},
+  methods: {
+    async showMessageFromBackend() {
+      try {
+        const response = (await this.$axios.get('/api/test')).data
+        this.randNumber = response.number
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+  },
+}
+</script>
